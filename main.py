5095006f424741c1491e3407a156e21baf628fa4
Bernd Hollerit <hollerit@gmail.com>
2024-03-20 10:09:33 +0900

    Initialize

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+Var HeadsOrTails()
+If(rand()<0.5
+  Return “H”
+Else
+  Return “T”
+
+A[101]
+A[0] = 0
+Alice = 0
+Bob =0
+for(i=1, i<=100, i++)
+A[i] = HeadsOrTails()
+if A[i] == H && A[i-1] == H
+  Alice += 1
+
+elseif A[i] == T && A[i-1] == H
+  Bob += 1
+
+Print Alice, Bob
