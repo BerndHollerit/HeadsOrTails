ca8e81e5f87069645e438f3be8e3899b6f05ebad
Bernd Hollerit <hollerit@gmail.com>
2024-03-20 11:01:35 +0900

    Add problem statement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Flip a fair coin 100 times - it gives a sequence of heads (H) and tails (T). For each HH in the sequence of flips,
+# Alice gets a point; for each HT, Bob does, so e.g. for the sequence THHHT Alice gets 2 points and Bob gets 1 point.
+# Who is most likely to win?
 import array
 import random
 
