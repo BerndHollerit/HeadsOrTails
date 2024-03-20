f9f6573c6407770f7ed1390f35677120ebde30c3
Bernd Hollerit <hollerit@gmail.com>
2024-03-20 10:40:29 +0900

    Implement algorithm

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
 
+
 # Randomly returns Heads or Tails
 def heads_or_tails():
     options = ['H', 'T']
@@ -20,24 +21,22 @@ def heads_or_tails():
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    heads_or_tails_array = array.array('u', [''] * 100)
+    heads_or_tails_array = array.array('u', [' '] * 100)
+    alice_score = 0
+    bob_score = 0
 
+    # Perform the first coin toss
+    heads_or_tails_array[0] = heads_or_tails()
+
+    for i in range(1, 100):
+        heads_or_tails_array[i] = heads_or_tails()
+
+        if heads_or_tails_array[i] == 'H' and heads_or_tails_array[i - 1] == 'H':
+            alice_score += 1
+        elif heads_or_tails_array[i] == 'T' and heads_or_tails_array[i - 1] == 'H':
+            bob_score += 1
+
+    print("Alice Score: {}".format(alice_score))
+    print("Bob Score: {}".format(bob_score))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-
-
-A[101]
-A[0] = 0
-Alice = 0
-Bob =0
-for(i=1, i<=100, i++)
-A[i] = HeadsOrTails()
-if A[i] == H && A[i-1] == H
-  Alice += 1
-
-elseif A[i] == T && A[i-1] == H
-  Bob += 1
-
-Print Alice, Bob
